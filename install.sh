5197cf6a777407836fba5de5118d7a7a9ddc8906
Lowyy12 <luisvadillo2004@gmail.com>
2024-03-15 12:33:28 +0100

    primero

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt  install curl
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 21805A48E6CBBA6B991ABE76646193862B759810
+sudo add-apt-repository ppa:katharaframework/kathara
+sudo apt update
+sudo apt install kathara
+sudo apt install xterm
